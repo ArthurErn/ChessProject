ac4ef7e82903d5338cee4055ebedc5de347a9155
sys-arthur <arthurern48@gmail.com>
2021-12-17 10:04:56 -0300

    differentiating colors

--- ChessProject/Program.cs
+++ ChessProject/Program.cs
@@ -7,11 +7,20 @@ namespace ChessProject {
     class Program {
         static void Main() {
 
-            ChessPosition pos = new ChessPosition('c', 7);
+            try {
+                Board board = new Board(8, 8);
 
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosition());
-            Console.ReadLine();
+                board.insertPiece(new Rook(board, Color.Black), new Position(0, 0));
+                board.insertPiece(new Rook(board, Color.Black), new Position(2, 3));
+                board.insertPiece(new King(board, Color.Black), new Position(1, 3));
+
+                board.insertPiece(new Rook(board, Color.White), new Position(7, 5));
+                board.insertPiece(new King(board, Color.White), new Position(5, 3));
+
+                Layout.printBoard(board);
+            } catch (BoardException e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- ChessProject/layout/Layout.cs
+++ ChessProject/layout/Layout.cs
@@ -4,17 +4,32 @@ using System;
 namespace layout {
     class Layout {
         public static void printBoard(Board board) {
+            
             for (int i  = 0; i < board.linhas; i++) {
+                Console.Write(8 - i + "|  ");
                 for (int j = 0; j < board.colunas; j++) {
                     if (board.piece(i, j) == null) {
                         Console.Write(" -  ");
                     } else {
-                        Console.Write(board.piece(i, j) + " ");
+                        Layout.printPiece(board.piece(i, j));
+                        Console.Write(" ");
                     }
                     
                 }
                 Console.WriteLine();
                 Console.WriteLine();
+            }
+            Console.WriteLine("     A   B   C   D   E   F   G   H");
+        }
+
+        public static void printPiece(Piece piece) {
+            if (piece.color == Color.White) {
+                Console.Write(piece);
+            } else {
+                ConsoleColor defaultColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(piece);
+                Console.ForegroundColor = defaultColor;
             }
         }
     }
