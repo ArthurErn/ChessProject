6f9f8bc06f5f5b62cc27099c469a957fcdd723a3
sys-arthur <arthurern48@gmail.com>
2021-12-17 09:56:22 -0300

    class ChessPosition

--- ChessProject/Program.cs
+++ ChessProject/Program.cs
@@ -7,17 +7,11 @@ namespace ChessProject {
     class Program {
         static void Main() {
 
-            try {
-                Board board = new Board(8, 8);
+            ChessPosition pos = new ChessPosition('c', 7);
 
-                board.insertPiece(new Rook(board, Color.Black), new Position(0, 0));
-                board.insertPiece(new Rook(board, Color.Black), new Position(2, 9));
-                board.insertPiece(new King(board, Color.Black), new Position(1, 3));
-
-                Layout.printBoard(board);
-            } catch (BoardException e) {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosition());
+            Console.ReadLine();
         }
     }
 }
---
+++ ChessProject/chess/ChessPosition.cs
@@ -0,0 +1,21 @@
+﻿using board;
+
+namespace chess {
+    class ChessPosition {
+        public ChessPosition(char coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public Position toPosition() {
+            return new Position(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString() {
+            return "" + coluna + linha;
+        }
+    }
+}
